3997b2ea27215b73ec970116201c5efe94957ddc
tbscanlon <thomas.scanlon@outlook.com>
2016-11-22 11:38:17 +0000

    Initial commit

---
+++ image-resize.py
@@ -0,0 +1,35 @@
+import os, sys
+from PIL import Image
+from glob import glob
+
+basewidth = 200 # set the size of the output image
+
+# Print instructions if no args are supplied
+if len(sys.argv) <= 1:
+	print "HOW TO USE:"
+	print "image-resize.py name-of-file.jpg"
+	exit(0)
+
+for infile in glob(sys.argv[1]):
+
+	# skip if the file has already been resized
+	if "resize" in infile:
+		print "Skipping %s as it has already been resized" % infile
+		continue
+
+	outfile = os.path.splitext(infile)[0] + "-resize.jpg" # create output files for each arg
+
+	if infile != outfile:
+		try:
+			im = Image.open(infile)
+			wpercent = (basewidth / float(im.size[0]))
+			hsize = int((float(im.size[1]) * float(wpercent)))
+			im = im.resize((basewidth, hsize), Image.ANTIALIAS)
+			im.save(outfile, "JPEG")
+
+			print "Successfully resized '%s'!" % infile
+			print "The resized image can be found at '%s'" % outfile
+			os.remove(infile)
+
+		except IOError:
+			print "cannot create thumbnail for '%s' - is it a valid filename?" % infile
